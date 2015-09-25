198f03599a1de5435d094ce0f3887454987f19e0
Perry Eising <perry.r.eising@gmail.com>
2015-09-25 13:34:25 -0700

    add passing tests to check for size, quanitity and proper pricing of pizza object

--- spec/specs.js
+++ spec/specs.js
@@ -1,26 +1,37 @@
 describe('Pizza', function() {
   it("instantiates as a pizza object", function() {
-    var testPizza = new Pizza(12, [1,2,3], 1);
-    expect(Pizza()).to.be.instanceOf(Pizza);
+    var testPizza = new Pizza(12, ["Red Sauce","Black Olives","Ham"], 1);
+    expect(testPizza).to.be.instanceOf(Pizza);
   });
 
   it("creates new pizza that is the correct size", function() {
-    var testPizza = new Pizza(12, [1,2,3], 1);
-    expect(Pizza.size).to.equal(12);
+    var testPizza = new Pizza(12, ["Red Sauce","Black Olives","Ham"], 1);
+    expect(testPizza.size).to.equal(12);
   });
 
   it("creates new pizza that has the correct toppings", function() {
-    var testPizza = new Pizza(12, [1,2,3], 1);
-    expect(Pizza.toppings).to.equal([1,2,3]);
+    var testPizza = new Pizza(12, ["Red Sauce","Black Olives","Ham"], 1);
+    expect(testPizza.toppings).to.eql(["Red Sauce","Black Olives","Ham"]);
   });
 
   it("matches the toppings in the array to actual toppings correctly", function() {
-    var testPizza = new Pizza(12, [1,2,3], 1);
-    expect(Pizza.toppings.name[0]).to.equal("red sauce");
+    var testPizza = new Pizza(12, ["Red Sauce","Black Olives","Ham"], 1);
+    expect(testPizza.toppings[0]).to.equal("Red Sauce");
   });
 
   it("creates the correct quantity of the pizza", function() {
-    var testPizza = new Pizza(12, [1,2,3], 1);
-    expect(Pizza.quantity).to.equal(1);
+    var testPizza = new Pizza(12, ["Red Sauce","Black Olives","Ham"], 1);
+    expect(testPizza.quantity).to.equal(1);
   });
+
+  it("correctly adds the price for added toppings", function() {
+    var testPizza = new Pizza(12, ["Red Sauce","Black Olives","Ham"], 1);
+    expect(testPizza.calcToppingsPrice()).to.equal(1.5);
+  });
+
+  it("correctly returns the base price for a pizza depending on size", function() {
+    var testPizza = new Pizza(12, ["Red Sauce","Regular Cheese"], 1); //these toppings have price = 0
+    expect(testPizza.calcPrice()).to.equal(11.99);
+  });
+
 });
